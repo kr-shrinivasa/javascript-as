09b743dd5b9006c7a5c4201d7e89f0b96791edd0
krshrinivasa <krshrinivasa@gmail.com>
2021-12-07 19:01:33 +0530

    create

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,12 @@
 function CreateObject(arr) {
-    // Write your code here
+    const obj = {}
+    for(var i=0;i<arr.length ;i+=2)
+    {
+        obj[arr[i]]=arr[i+1]
+    }
+    return obj
 }
+    // Write your code here
+
 
 module.exports = CreateObject;
