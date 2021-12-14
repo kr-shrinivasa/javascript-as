890e776d08f50c9215962c477ae9f710d949b5d7
krshrinivasa <krshrinivasa@gmail.com>
2021-12-14 11:35:58 +0530

    todo

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,12 +1,5 @@
 function CreateObject(arr) {
-    const obj = {}
-    for(var i=0;i<arr.length ;i+=2)
-    {
-        obj[arr[i]]=arr[i+1]
-    }
-    return obj
-}
     // Write your code here
-
+}
 
 module.exports = CreateObject;
---
+++ assignments/todo_list/script.js
@@ -0,0 +1,31 @@
+const todoForm = document.querySelector(".form-todo")
+const todoinput = document.querySelector(".form-todo input[type='text']")
+console.log(todoinput)
+const ul =document.querySelector(".todos")
+
+todoForm.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    const newli=document.createElement("div")
+    const insideNewli=`<span class="text">${todoinput.value}</span>
+    <div>
+        <button class="todo-btn done">Done</button>
+        <button class="todo-btn remove">Remove</button>
+
+    </div>`
+    newli.innerHTML=insideNewli
+    // console.log(todoinput.value);
+    ul.append(newli)
+    todoinput.value="";
+})
+
+ul.addEventListener("click",(e)=>{
+    if (e.target.classList.contains("remove")){
+        const removeitem= e.target.parentNode.parentNode
+        removeitem.remove()
+    }
+    if (e.target.classList.contains("done")){
+        const lispan =e.target.parentNode.previousElementSibling;
+        // console.log(lispan)
+        lispan.style.textDecoration ="line-through"
+    }
+})
